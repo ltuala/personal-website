25993d8ec5a1680b2546cd1f39777edcf1652de0
ltuala <lyndontuala28@gmail.com>
2023-06-29 00:37:08 +0800

    update baseUrl on docusauru.config.js

--- docusaurus-try-website/docusaurus.config.js
+++ docusaurus-try-website/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://piga-sandbox.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/l-personal-website',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
